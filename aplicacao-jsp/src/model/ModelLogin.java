5bd19ba38e33f92156d774cff124fa8ce9a51017
Ronaldo2017 <Ronaldo2017@users.noreply.github.com>
2022-04-05 11:51:20 -0300

    Foram feitas várias modificações, e incluido a classe de usuario e busca, também feio modal para fazer a pesquisa no BD.

--- aplicacao-jsp/src/model/ModelLogin.java
+++ aplicacao-jsp/src/model/ModelLogin.java
@@ -2,20 +2,57 @@ package model;
 
 import java.io.Serializable;
 
-public class ModelLogin implements Serializable{
+public class ModelLogin implements Serializable {
 
 	/**
 	 * compilação das classes
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
+	private Long id;
+	private String nome;
+	private String email;
 	private String login;
 	private String senha;
 	
+	public boolean isNovo() {
+		if(this.id == null) {
+			return true;//inserir novo
+		}else if(this.id != null && this.id > 0) {
+			return false;//atualiza
+		}
+
+		return id == null;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-	
+
 	public String getSenha() {
 		return senha;
 	}
@@ -27,7 +64,5 @@ public class ModelLogin implements Serializable{
 	public void setLogin(String login) {
 		this.login = login;
 	}
-	
-	
 
 }
---
+++ aplicacao-jsp/src/servlets/ServletUsuarioController.java
@@ -0,0 +1,110 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.DAOUsuarioRepository;
+import model.ModelLogin;
+
+/**
+ * Servlet implementation class ServletUsuarioController
+ */
+@WebServlet("/ServletUsuarioController")
+public class ServletUsuarioController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private DAOUsuarioRepository daoUsuarioRepository = new DAOUsuarioRepository();
+
+	public ServletUsuarioController() {
+		super();
+	}
+
+	// para consultar e deletar usa o Get
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String acao = request.getParameter("acao");
+
+		try {
+			if (acao != null && !acao.isEmpty() && acao.equalsIgnoreCase("deletar")) {
+				String idUser = request.getParameter("id");
+				daoUsuarioRepository.deletarUser(idUser);
+				request.setAttribute("msg", "Excluído com sucesso!");
+				request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
+			} else if (acao != null && !acao.isEmpty() && acao.equalsIgnoreCase("deletarajax")) {
+				String idUser = request.getParameter("id");
+				daoUsuarioRepository.deletarUser(idUser);
+
+				response.getWriter().write("Excluído com sucesso!");
+			} else if (acao != null && !acao.isEmpty() && acao.equalsIgnoreCase("buscarUserAjax")) {
+				String nomeBusca = request.getParameter("nomeBusca");
+				System.out.println(nomeBusca);
+				// daoUsuarioRepository.deletarUser(idUser);
+
+//				response.getWriter().write("Excluído com sucesso!");
+			} else {
+				request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			RequestDispatcher redirecionar = request.getRequestDispatcher("erro.jsp");
+			request.setAttribute("msg", e.getMessage());
+			redirecionar.forward(request, response);
+		}
+
+	}
+
+	// para gravar e atualizar usa o Post
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String msg = "Operação realizada com sucesso!";
+
+		try {
+			// recuperando dados do usuario
+			String id = request.getParameter("id");// vem da tela como string
+			String nome = request.getParameter("nome");
+			String email = request.getParameter("email");
+			String login = request.getParameter("login");
+			String senha = request.getParameter("senha");
+
+			// iniciando o objeto/ injeta os dados vindos da tela
+			ModelLogin modelLogin = new ModelLogin();
+			modelLogin.setId(id != null && !id.isEmpty() ? Long.parseLong(id) : null);
+			modelLogin.setNome(nome);
+			modelLogin.setEmail(email);
+			modelLogin.setLogin(login);
+			modelLogin.setSenha(senha);
+
+			if (daoUsuarioRepository.validarLogin(modelLogin.getLogin()) && modelLogin.getId() == null) {
+				msg = "Já existe um usuário com o mesmo login, informe outro login!";
+			} else if (modelLogin.isNovo()) {
+				msg = "Gravado com seucesso!";
+			} else {
+				msg = "Atualizado com sucesso!";
+			}
+
+			modelLogin = daoUsuarioRepository.gravarUsuario(modelLogin);
+			request.setAttribute("msg", msg);
+
+			// manter os dados na tela, ou fazer edicao dos dados
+			request.setAttribute("modelLogin", modelLogin);
+
+			// redireciona a tela para usuario.jsp
+			request.getRequestDispatcher("principal/usuario.jsp").forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+			RequestDispatcher redirecionar = request.getRequestDispatcher("erro.jsp");
+			request.setAttribute("msg", e.getMessage());
+			redirecionar.forward(request, response);
+		}
+	}
+
+}
